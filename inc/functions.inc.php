e7971acd61914a52c1d58acbad789691ecd27348
hguochen <hguochen@gmail.com>
2012-12-04 02:01:06 +0800

    Edit function.inc.php
    In case no entries were returned, display a default message with a link
    to post an entry

--- inc/functions.inc.php
+++ inc/functions.inc.php
@@ -16,6 +16,14 @@
 			}
 			//Set the fulldisp flag for multiple entries
 			$fulldisp = 0;
+			
+			//If no entries were returned, display a default message and set the fulldisp flag to display a single entry
+			if(!is_array($e)) {
+				$fulldisp = 1;
+				$e = array(
+						'title' => 'No Entries Yet',
+						'entry' => '<a href="../admin.php">Post an entry!</a>');
+			}
 		}
 	}
 ?>
