0a1f24c8a5b8b7e0826aff0d7d03d21232bfa6f6
hguochen <hguochen@gmail.com>
2012-12-04 01:33:54 +0800

    Added index.php file
    added structure of html page in index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html
+	PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<meta http-equiv="Content-Type"
+		content="text/html;charset=utf-8" />
+	<link rel="stylesheet" href="css/default.css" type="text/css" />
+	<title> Simple Blog </title>
+</head>
+
+<body>
+	<h1> Simple Blog Application </h1>
+	<div id="entries">
+<?php
+// Format the entries from the database
+?>
+	<p class="backlink">
+		<a href="/admin.php">Post a New Entry</a>
+	</p>
+
+	</div>
+</body>
+
+</html>
