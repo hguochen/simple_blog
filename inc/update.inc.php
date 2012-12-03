b581f5ece7e0d10152d430d16f71a3838e464241
hguochen <hguochen@gmail.com>
2012-12-04 01:14:41 +0800

    edit update.inc.php
    formulate queries line 15

--- inc/update.inc.php
+++ inc/update.inc.php
@@ -1,6 +1,4 @@
 <?php
-//4. Connect to the database
-//5. Formulate a MySQL query to store the entry data
 //6. Sanitize the input and store it in the entries table
 //7. Obtain the unique ID for the newly created entry
 //8. Send the user to the newly created entry
@@ -8,7 +6,21 @@ if($_SERVER['REQUEST_METHOD']=='POST'
 		&& $_POST['submit']=='Save Entry'
 		&& !empty($_POST['title'])
 		&& !empty($_POST['entry'])) {
+	
+	//Include database credentials and connect to the database
+	include_once 'db.inc.php';
+	$db = new PDO(DB_INFO, DB_USER, DB_PASS);
+	
+	//Save the entry into the database
+	$sql = "INSERT INTO entries 
+			(title,entry) 
+			VALUES(?,?)";
+	$stmt = $db->prepare($sql);
+	$stmt->execute(array($title,$entry));
+	$stmt->closeCursor();//close the cursor, enable the statement to be executed again
+	
 	//Continue processing information
+	
 } else {
 	
 	//If both conditions aren't met, sends the user back to the main page
